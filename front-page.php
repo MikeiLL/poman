daceada28a3458fbef1a7043329701f41720d02f
Mike iLL Kilmer <mike@mzoo.org>
2015-11-17 16:32:22 -0600

    Style the produce a bit.

--- front-page.php
+++ front-page.php
@@ -7,12 +7,10 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php //get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
-  
+
   <!-- row for wells -->
 		<div class="row col-wrap">
-			<?php get_template_part('templates/front-page/coming'); ?>
-		</div><!-- end row -->
-		
-
+      <?php get_template_part('templates/front-page/produce'); ?>
+		</div><!-- /.row col-wrap first row-->
 
 <?php endwhile; ?>
